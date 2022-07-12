75c8af554e84dc0ce402a8223b07c4f16515b745
iisaka51@gmail.com <goichiiisaka@GoichiMacBook.local>
2022-07-12 12:40:31 +0900

    update: add span(), floor(), ceil()

--- Tutorials/Tutorial.Datetime/06_Arrow/c20_range_span.py
+++ Tutorials/Tutorial.Datetime/06_Arrow/c20_range_span.py
@@ -5,3 +5,11 @@ end = arrow.get('2022-01-01 02:00')
 
 for r in arrow.Arrow.range('hour', start, end):
   print(repr(r))
+
+dt = arrow.get('2020-02-01 00:00')
+dt.span('hour')
+dt.span('month')
+
+dt = arrow.get('2020-02-14 00:00')
+dt.floor('month')
+dt.ceil('month')
